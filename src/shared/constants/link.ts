56ed37220c821360936bd493204fca45cf54c3de
Map1en <maplenagisa@gmail.com>
2026-05-22 23:37:04 +0900

    fix: QQ group link to constants

--- src/shared/constants/link.ts
+++ src/shared/constants/link.ts
@@ -3,7 +3,8 @@ const links: any = {
     github: 'https://github.com/Map1en/VRCX-0',
     issues: 'https://github.com/Map1en/VRCX-0/issues',
     releases: 'https://github.com/Map1en/VRCX-0/releases',
-    discord: 'https://discord.gg/bnEVqwSp'
+    discord: 'https://discord.gg/bnEVqwSp',
+    qqGroup: 'https://qm.qq.com/q/MDK8QDUX2C'
 };
 
 export { links };
